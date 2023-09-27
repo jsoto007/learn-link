09791a407142cecce90fa7addf15fc5fb5c4c15f
Nicholas <crazedglitch@yahoo.com>
2023-09-27 12:23:24 -0500

    test seed data created for user and course

--- server/seed.py
+++ server/seed.py
@@ -15,3 +15,43 @@ if __name__ == '__main__':
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+
+        print( "Deleting data..." )
+        User.query.delete()
+        Course.query.delete()
+        Lesson.query.delete()
+
+        print( "Creating users..." )
+        u1 = User(
+            email = "nicholas@gmail.com",
+            password_hash = "password",
+            username = "Glitchy",
+            first_name = "Nicholas",
+            last_name = "Martin",
+            avatar = "https://vdostavka.ru/wp-content/uploads/2019/05/no-avatar.png",
+            bio = "Random Words to fill the void."
+        )
+
+        u2 = User(
+            email = "jose@gmail.com",
+            password_hash = "password",
+            username = "J-Man",
+            first_name = "Jose",
+            last_name = "Soto",
+            avatar = "https://vdostavka.ru/wp-content/uploads/2019/05/no-avatar.png",
+            bio = "Random Words to fill the void.",
+        )
+        db.session.add_all( [ u1, u2 ] )
+        db.session.commit()
+        print( 'Users created successfully!' )
+
+        print( 'Creating Courses...' )
+        c1 = Course(
+            title = "Mathematics",
+            description = "Basic math course",
+            score = False,
+            start_date = None,
+            end_date = None,
+            user_id = u1.id,
+            lesson_id = l1.id,
+        )
