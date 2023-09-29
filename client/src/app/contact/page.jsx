fea72b1f8383ff616b93be0c534df22e1fdffb15
JedwardMook <jedwardmook@yahoo.com>
2023-09-28 20:31:42 -0500

    Adding modal to Contact page and trying to figure state

--- client/src/app/contact/page.jsx
+++ client/src/app/contact/page.jsx
@@ -2,22 +2,24 @@
 import React, { useState } from 'react'
 
 export default function Contact() {
-    const [isSent, setIsSent] = useState(false)
+    const [isNotSent, setIsNotSent] = useState(true)
+
+    function handleSent(){
+        setIsNotSent(!isNotSent);
+        console.log("I was clicked");
+    }
+
   return (
     <main className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div className="sm:mx-auto sm:w-full sm:max-w-md">
-    {/* <img
-      className="mx-auto h-10 w-auto"
-      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-      alt="Your Company"
-    /> */}
+
     <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
       Contact Us
     </h2>
     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
       <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
         <p className="text-stone-500 mb-4">Please use the form below to contact us with any thoughts, concerns, or ideas of how to improve Learn Link.</p>
-          <form className="space-y-6" action="#" method="POST">
+          <form className="space-y-6" onSubmit={handleSent}>
           <div>
             <label htmlFor="name" className="block text-sm font-medium text-gray-900">
               Name
@@ -42,7 +44,6 @@ export default function Contact() {
                 id="email"
                 name="email"
                 type="email"
-                autoComplete="email"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -54,7 +55,7 @@ export default function Contact() {
               Message
             </label>
             <div className="mt-1">
-              <textbox
+              <textarea
                 id="message"
                 name="message"
                 required
@@ -63,29 +64,8 @@ export default function Contact() {
             </div>
           </div>
 
-          {/* <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                id="remember-me"
-                name="remember-me"
-                type="checkbox"
-                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-              />
-              <label htmlFor="remember-me" className="ml-3 block text-sm leading-6 text-gray-900">
-                Remember me
-              </label>
-            </div>
-
-            <div className="text-sm leading-6">
-              <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                Forgot password?
-              </a>
-            </div>
-          </div> */}
-
           <div>
-            <button
-              type="submit"
+          <button
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Send Message
@@ -94,7 +74,38 @@ export default function Contact() {
         </form>
       </div>
     </div>
+    </div> : 
+    {/* <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="false">
+
+  <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+  <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+    <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+      
+      <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+        <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+          <div class="sm:flex sm:items-start">
+            <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+              <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+              </svg>
+            </div>
+            <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+              <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Deactivate account</h3>
+              <div class="mt-2">
+                <p class="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+          <button type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Deactivate</button>
+          <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
+        </div>
+      </div>
     </div>
+  </div>
+</div> */}
   </main>
   )
 }
