adc9645bf0550da4fa1ddb224677ad1901a13797
Sterling <sterlingbarton81@gmail.com>
2023-10-03 11:32:16 -0600

    completed intro and outro pages for questionnaire

--- client/src/app/signup/welcome/page.jsx
+++ client/src/app/signup/welcome/page.jsx
@@ -1,23 +1,23 @@
-export default function Thankyou() {
+export default function Welcome() {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Thank You!
+          Hi, I'm Adda
         </h2>
-        <p>You did it! Thanks for taking the time to share your thoughts with me. Your input is incredibly valuable, and I can't wait to use it to create the perfect learning experience just for you.
-          <br>Stay tuned for your amazing journey ahead!</br>
-        </p>
+        <p>I’m thrilled to have you on board, and your unique journey is my top priority. To ensure I provide you with the best learning experience tailored to your needs, I have a few questions for you. Don't worry; it's quick and easy!</p>
         <div className="mt-10 flex items-center gap-x-6">
           <a
-            href="/dashboard"
+            href="/signup/questionnaire"
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Get started
+            I'm ready
+          </a>
+          <a href="/dashboard" className="text-sm font-semibold leading-6 text-gray-900">
+            No questions, please <span aria-hidden="true">→</span>
           </a>
         </div>
       </div>
     </div>
   )
 }
-
