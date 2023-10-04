63b39e0ba4042b30c7230b5436acd3fc3bed1bc1
Jose Soto <jsoto7087@gmail.com>
2023-10-04 14:24:38 -0400

    side nav bar

--- client/src/app/components/UserDashboard.jsx
+++ client/src/app/components/UserDashboard.jsx
@@ -1,4 +1,10 @@
 import Image from "next/image"
+import user from "../../../public/user.svg"
+import book from "../../../public/book.svg"
+import trending from "../../../public/trending.svg"
+import sliders from "../../../public/sliders.svg"
+import logOut from "../../../public/logOut.svg"
+
 
 export default function UserDashboard() {
    
@@ -10,7 +16,67 @@ export default function UserDashboard() {
             className="h-20 mx-auto object-cover rounded-full w-20 border-2"
             src="https://images.pexels.com/photos/7275385/pexels-photo-7275385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
           />
+          <div className="sm:mt-10 dm:mt-10 lg:mt-20">
+          <Image
+            src={user}
+            width={40}
+            height="auto"
+            className="ml-5 mb-2"
+          />
+          <spam className="flex justify-center text-white text-sm">
+            Profole
+          </spam>
           
+          </div>
+          <div className="sm:mt-4 dm:mt-6 lg:mt-10">
+          <Image
+            src={book}
+            width={40}
+            height="auto"
+            className="ml-5 mb-2"
+          />
+          <spam className="flex justify-center text-white text-sm">
+            Lessons
+          </spam>
+          
+          </div>
+
+          <div className="sm:mt-4 dm:mt-6 lg:mt-10">
+            <Image
+              src={trending}
+              width={40}
+              height="auto"
+              className="ml-5 mb-2"
+            />
+            <spam className="flex justify-center text-white text-sm">
+              My Progress
+            </spam>
+          </div>
+
+          <div className="sm:mt-4 dm:mt-6 lg:mt-10">
+            <Image
+              src={sliders}
+              width={40}
+              height="auto"
+              className="ml-5 mb-2"
+            />
+            <spam className="flex justify-center text-white text-sm">
+              Accessibility
+            </spam>
+          </div>
+
+          
+          {/* <div className="flex sm:mt-8 dm:mt-12 lg:mt-20 text-sm">
+            <Image
+              src={logOut}
+              width={40}
+              height="auto"
+              className="ml-5 mb-2"
+            />
+            <spam className="flex items-center text-white text-sm ml-2">
+              Log out
+            </spam>
+          </div> */}
         </div>
       </div>
      
